c0f2d9a8e4e7b88f9c0d634da2b7eaa2a61cee77
Noé Tourbillon <noe.tourbillon@etu.unicaen.fr>
2024-02-18 15:23:53 +0100

    bugfix: Preview can be seen

--- app/src/main/java/com/chordsGenerator/app/AudioPlayerForMediaPlayer.kt
+++ app/src/main/java/com/chordsGenerator/app/AudioPlayerForMediaPlayer.kt
@@ -2,18 +2,18 @@ import android.content.Context
 import android.media.MediaPlayer
 
 class AudioPlayerForMediaPlayer(private val context: Context) : AudioPlayer {
-    private var mediaPlayer: MediaPlayer = MediaPlayer()
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun play(audioResourceId: Int) {
         stop()
         mediaPlayer = MediaPlayer.create(context, audioResourceId)
-        mediaPlayer.start()
+        mediaPlayer?.start()
     }
 
     override fun stop() {
-        if(mediaPlayer.isPlaying){
-            mediaPlayer.stop()
+        if(mediaPlayer?.isPlaying == true){
+            mediaPlayer?.stop()
         }
-        mediaPlayer.release()
+        mediaPlayer?.release()
     }
 }
